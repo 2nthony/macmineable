fb934c90d3a20b46dd62924fc6bf647cf1128c76
evillt <ijoec123@gmail.com>
2021-08-25 17:39:09 +0800

    feat(icon-refresh): icon-park

--- client/src/components/icons/Refresh.svelte
+++ client/src/components/icons/Refresh.svelte
@@ -11,20 +11,20 @@
   xmlns:xlink="http://www.w3.org/1999/xlink"
   aria-hidden="true"
   role="img"
-  class={`iconify iconify--feather ${className}`}
+  class={`iconify iconify--icon-park ${className}`}
   width="32"
   height="32"
   preserveAspectRatio="xMidYMid meet"
-  viewBox="0 0 24 24"
+  viewBox="0 0 48 48"
   on:click={click}
   ><g
     fill="none"
-    stroke="currentColor"
-    stroke-width="2"
+    stroke="#000"
+    stroke-width="4"
     stroke-linecap="round"
     stroke-linejoin="round"
-    ><path d="M23 4v6h-6" /><path d="M1 20v-6h6" /><path
-      d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"
+    ><path
+      d="M42 8v16M6 24v16M6 24c0 9.941 8.059 18 18 18a17.94 17.94 0 0 0 12.5-5.048M42 24c0-9.941-8.058-18-18-18a17.947 17.947 0 0 0-12.951 5.5"
     /></g
   ></svg
 >
