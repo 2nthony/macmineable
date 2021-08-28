d9172dede9278c4624ba902ab9c62eb34a6d9b41
evillt <ijoec123@gmail.com>
2021-08-28 15:23:00 +0800

    refactor(go): ipc

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"runtime"
 
 	"github.com/asaskevich/EventBus"
 	"github.com/evillt/webview"
-	"github.com/pkg/browser"
 )
 
 var eventbus = EventBus.New()
@@ -58,67 +54,4 @@ func createServer() {
 	go func() {
 		fmt.Println(http.ListenAndServe(fmt.Sprintf("%s:%s", hostIP, hostPort), nil))
 	}()
-}
-
-// client events
-func registerIPCEvents(w webview.WebView) {
-	var miningProcess *exec.Cmd
-	minerPath := Ternay(IsIntel(), "assets/miner/xmrig", "assets/miner/xmrig-m1")
-
-	w.Bind("emitPageReady", func() {
-		fmt.Println("emitPageReady")
-		w.Eval(fmt.Sprintf(`
-        onPageReady({
-          cpuCores: %s
-        })
-        `,
-			fmt.Sprint(runtime.NumCPU()),
-		))
-	})
-
-	type Form struct {
-		Symbol       string `json:"symbol"`
-		Address      string `json:"address"`
-		ReferralCode string `json:"referralCode"`
-		CPUUsage     int    `json:"cpuUsage"`
-	}
-	w.Bind("emitStartMining", func(data string) {
-		var form Form
-		json.Unmarshal([]byte(data), &form)
-
-		fmt.Printf("form: %v\n", form)
-
-		if miningProcess != nil {
-			w.Eval("onMiningStarted()")
-			return
-		}
-
-		process, err := RunCommand(
-			fmt.Sprintf(`%s --no-color --url=rx.unmineable.com:3333 --algo=rx --pass=x --keepalive --user=%s:%s.macMineable#%s --cpu-max-threads-hint=%s`, minerPath, form.Symbol, form.Address, form.ReferralCode, fmt.Sprint(form.CPUUsage)),
-		)
-		if err != nil {
-			w.Eval(fmt.Sprintf(`onMiningStartedError("%s")`, err))
-			return
-		}
-
-		w.Eval("onMiningStarted()")
-
-		miningProcess = process
-	})
-	w.Bind("emitStopMining", func() {
-		if miningProcess != nil {
-			// prefer kill the process
-			miningProcess.Process.Kill()
-			miningProcess = nil
-			w.Eval("onMiningStopped()")
-			/* err := miningProcess.Process.Signal(os.Interrupt)
-						if err != nil {
-							w.Eval(fmt.Sprintf("onMiningStoppedError(%s)", err))
-						} else {
-			      } */
-		}
-	})
-	w.Bind("emitOpenURL", func(url string) {
-		browser.OpenURL(url)
-	})
 }
