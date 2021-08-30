6082b56b65f460d8789441c180fe3339c9945d00
evillt <ijoec123@gmail.com>
2021-08-30 20:52:16 +0800

    feat(ui): better websocket
    reconnect. ping

--- client/src/helper/wsMiningLog.js
+++ client/src/helper/wsMiningLog.js
@@ -1,10 +1,15 @@
+import { onDestroy } from 'svelte'
+import { listen } from 'svelte/internal'
 import { useWebsocket } from '../use/websocket'
 import { miningLogs } from '../store'
+import { createCounter } from '../util/counter'
 
 export function wsMiningLog() {
-  const { message: onMiningLog } = useWebsocket(
-    `ws://${location.host}/wsMiningLog`,
-  )
+  const {
+    message: onMiningLog,
+    ping,
+    reconnect,
+  } = useWebsocket(`ws://${location.host}/mining-log`)
 
   onMiningLog((log) => {
     miningLogs.update((val) => {
@@ -17,4 +22,21 @@ export function wsMiningLog() {
       return val
     })
   })
+
+  // TODO this may be reason of unresponsive
+  createCounter(10, ping)
+
+  const unlisten = listen(
+    document,
+    'visibilitychange',
+    () => {
+      const visible = document.visibilityState === 'visible'
+      if (visible) {
+        reconnect()
+      }
+    },
+    false,
+  )
+
+  onDestroy(unlisten)
 }
--- client/src/use/websocket.js
+++ client/src/use/websocket.js
@@ -3,9 +3,37 @@ import { log } from '../util/log'
 /** @type {WebSocket} */
 let ws
 
-export function useWebsocket(url, protocol) {
-  const onMessage = []
+const onMessage = []
 
+export function useWebsocket(url, protocol) {
+  connect(url, protocol)
+
+  return {
+    message(fn) {
+      onMessage.push(fn)
+    },
+
+    ping: () => {
+      if (!ws) return
+      ws.send('ping')
+    },
+    reconnect: () => {
+      if (ws) return
+      connect(url, protocol)
+    },
+  }
+}
+
+function invokeAll(fns, data) {
+  if (!ws) {
+    log('ws', 'ws is not available')
+    return
+  }
+
+  fns.forEach((fn) => fn(data))
+}
+
+function connect(url, protocol) {
   ws = new WebSocket(url, protocol)
 
   ws.onmessage = (event) => {
@@ -18,19 +46,4 @@ export function useWebsocket(url, protocol) {
     log('ws', 'closed.')
     ws = null
   }
-
-  return {
-    message(fn) {
-      onMessage.push(fn)
-    },
-  }
-}
-
-function invokeAll(fns, data) {
-  if (!ws) {
-    log('ws', 'ws is not available')
-    return
-  }
-
-  fns.forEach((fn) => fn(data))
 }
