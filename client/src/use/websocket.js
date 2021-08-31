0ce62d2dfd4d26a518ba350ff8e5f7544ae4457e
evillt <ijoec123@gmail.com>
2021-08-31 17:30:48 +0800

    refactor(ui): use/websocket private vars

--- client/src/use/websocket.js
+++ client/src/use/websocket.js
@@ -1,12 +1,36 @@
 import { log } from '../util/log'
 
-/** @type {WebSocket} */
-let ws
-
-const onMessage = []
-
 export function useWebsocket(url, protocol) {
+  /** @type {WebSocket} */
+  let ws
+
+  const onMessage = []
+
   connect(url, protocol)
+
+  function invokeAll(fns, data) {
+    if (!ws) {
+      log('ws', 'ws is not available')
+      return
+    }
+
+    fns.forEach((fn) => fn(data))
+  }
+
+  function connect(url, protocol) {
+    ws = new WebSocket(url, protocol)
+
+    ws.onmessage = (event) => {
+      invokeAll(onMessage, event.data)
+    }
+    ws.onopen = () => {
+      log('ws', 'opened.')
+    }
+    ws.onclose = () => {
+      log('ws', 'closed.')
+      ws = null
+    }
+  }
 
   return {
     message(fn) {
@@ -21,29 +45,5 @@ export function useWebsocket(url, protocol) {
       if (ws) return
       connect(url, protocol)
     },
-  }
-}
-
-function invokeAll(fns, data) {
-  if (!ws) {
-    log('ws', 'ws is not available')
-    return
-  }
-
-  fns.forEach((fn) => fn(data))
-}
-
-function connect(url, protocol) {
-  ws = new WebSocket(url, protocol)
-
-  ws.onmessage = (event) => {
-    invokeAll(onMessage, event.data)
-  }
-  ws.onopen = () => {
-    log('ws', 'opened.')
-  }
-  ws.onclose = () => {
-    log('ws', 'closed.')
-    ws = null
   }
 }
