c20b73dd3406321c731ad92a3eb21fd69a4077b0
evillt <ijoec123@gmail.com>
2021-09-07 02:35:34 +0800

    fix: use legacy plugin to support old browser. e.g. safari 10.x

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
 import sveltePreprocess from 'svelte-preprocess'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,6 @@ export default defineConfig({
     svelte({
       preprocess: sveltePreprocess(),
     }),
+    legacy(),
   ],
 })
