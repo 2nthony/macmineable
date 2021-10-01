e26b44618ac2f393098cf41a4675c71c0121254f
evillt <ijoec123@gmail.com>
2021-10-01 21:06:21 +0800

    chore: release v0.12.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,37 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
 import sveltePreprocess from 'svelte-preprocess'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: 'client',
-  build: {
-    outDir: path.join(__dirname, 'dist'),
-    emptyOutDir: true,
-  },
-  plugins: [
-    svelte({
-      preprocess: sveltePreprocess(),
-    }),
-  ],
+export default defineConfig(({ command }) => {
+  const config: UserConfig = {
+    root: 'client',
+    build: {
+      outDir: path.join(__dirname, 'dist'),
+      emptyOutDir: true,
+    },
+    resolve: {
+      alias: {}
+    },
+    plugins: [
+      svelte({
+        preprocess: sveltePreprocess(),
+      }),
+    ],
+  }
+
+  if (command !== 'build') {
+    config.resolve.alias = [
+      {
+        find: /^@svelte-use\/(.*)/,
+        replacement: path.resolve(
+          __dirname,
+          '../../../dev/svelte-use/packages/$1/dist/index.mjs',
+        ),
+      },
+    ]
+  }
+
+  return config
 })
