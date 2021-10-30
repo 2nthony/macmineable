ebf65bb5b302521023fa7a4baf3659fe6a99b837
evillt <ijoec123@gmail.com>
2021-10-30 16:04:19 +0800

    fix(client): [promotion] use githubusercontent

--- client/src/components/PromotionBanner.svelte
+++ client/src/components/PromotionBanner.svelte
@@ -2,8 +2,8 @@
 import { useAsyncState, now, readable } from '@svelte-use/core'
 import { openUrl } from '../directives/openUrl'
 
-const promotionConfigFileUrl = `https://cdn.jsdelivr.net/gh/2nthony/macmineable-promotion-service@latest/config.json?_=${now()}`
-const { state } = useAsyncState(
+const promotionConfigFileUrl = `https://raw.githubusercontent.com/2nthony/macmineable-promotion-service/main/config.json?_=${now()}`
+const { state, isReady } = useAsyncState(
   fetch(promotionConfigFileUrl).then((res) => res.json()),
   {
     imageUrl: '',
@@ -22,12 +22,14 @@ $: href = readable('', set => {
 </script>
 
 <a class="promotion-banner cursor-pointer" target="_blank" href={$href} use:openUrl>
-{#if !$state.imageUrl}
-  <div class="flex justify-center items-center w-full h-full bg-gray-100 dark:bg-gray-800 text-xs">
-    Promotion Banner
-  </div>
-{:else}
-  <img class="w-full h-full" src={$state.imageUrl} alt="">
+{#if $isReady}
+  {#if !$state.imageUrl}
+    <div class="flex justify-center items-center w-full h-full bg-gray-100 dark:bg-gray-800 text-xs">
+      Promotion Banner
+    </div>
+  {:else}
+    <img class="w-full h-full" src={$state.imageUrl} alt="">
+  {/if}
 {/if}
 </a>
 
